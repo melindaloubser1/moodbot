817da45d56401db5c04bce131cd55fa6532c2d4a
melindaloubser1 <melinda.loubser@gmail.com>
2021-10-18 11:34:20 +0200

    force deactivate form

---
+++ README.md
@@ -0,0 +1,5 @@
+## What is this branch about?
+
+`deactivate_form` is an example of deactivating a form from the FormValidationAction.
+In this case, it will deactivate the form and continue as if the user's intent was the second intent. 
+Just type `hi q` and it should continue as if the intent was `affirm`.
--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,59 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from logging import shutdown
+from typing import Text, List, Any, overload
+import logging
+from copy import deepcopy
+
+from rasa_sdk.types import DomainDict
+from rasa_sdk import FormValidationAction, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import (
+    SlotSet,
+    ActiveLoop,
+    ActionExecutionRejected,
+    UserUtteranceReverted,
+    UserUttered,
+)
+
+logger = logging.getLogger(__name__)
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidateGreetForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_greet_form"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def should_deactivate(self, tracker: Tracker) -> bool:
+        return "q" in tracker.latest_message.get("text")
+    
+    def get_second_intent_parse_data(self, tracker: Tracker) -> dict:
+        intent_ranking = tracker.latest_message.get("intent_ranking", [])
+        if len(intent_ranking) < 2:
+            return {}
+        return intent_ranking[1]
+
+    def compose_parse_data(self, tracker, override_intent_data):
+        parse_data = deepcopy(tracker.latest_message)
+        parse_data["intent"] = override_intent_data
+        parse_data["intent"]["confidence"] = 1.0
+        parse_data["intent_ranking"] = []
+        return parse_data
+
+    async def run(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> List[Any]:
+        if self.should_deactivate(tracker):
+            override_intent_data = self.get_second_intent_parse_data(tracker)
+            events = [
+                SlotSet("requested_slot", None),
+                SlotSet("name", None),
+                ActiveLoop(None),
+                ActionExecutionRejected(self.form_name()),
+                UserUttered(
+                    tracker.latest_message.get("text"), parse_data=self.compose_parse_data(tracker, override_intent_data)
+                ),
+            ]
+            return events
+
+        return await super().run(dispatcher, tracker, domain)
