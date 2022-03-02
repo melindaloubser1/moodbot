0eace1bd53392fb26785538ce474d93414c553de
melindaloubser1 <melinda.loubser@gmail.com>
2022-03-02 11:52:16 +0100

    logic that should only run on form entry

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,46 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List
+import logging
+
+from abc import ABC
+
+from rasa_sdk import FormValidationAction, Tracker
+from rasa_sdk.events import EventType
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+logger = logging.getLogger(__name__)
+
+class CustomFormValidationAction(FormValidationAction, ABC):
+    def name(self):
+        return
+
+    async def run(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> List[EventType]:
+        events = await super().run(dispatcher, tracker, domain)
+        logger.error("Run method!")
+        logger.error(f"active loop {tracker.active_loop}")
+
+        await self.extra_run_logic(dispatcher, tracker, domain, events)
+        return events
+
+    async def extra_run_logic(self, dispatcher, tracker, domain, events):
+        return
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidateIntroForm(CustomFormValidationAction):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "validate_intro_form"
+
+    async def extra_run_logic(self, dispatcher, tracker, domain, events):
+        if tracker.get_slot("requested_slot") is None:
+            dispatcher.utter_message(text="I am the extra run logic and I should run at the beginning of a form!")
+        events.extend(await self.validate(dispatcher, tracker, domain))
+
+        return events
+
+
